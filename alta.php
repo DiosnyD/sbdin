896320e504e8f866e1d0bbf3ad5ade5b5ed733e8
DiosnyD <84103430+DiosnyD@users.noreply.github.com>
2021-06-04 13:01:18 -0500

    Add files via upload

--- alta.php
+++ alta.php
@@ -1,6 +1,4 @@
 <?php include('head.php');?>
-<!DOCTYPE html>
-<html lang="en">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -82,7 +80,6 @@
   </form>
 </div>
 </Form>
-</html>
 <?php include('footer.php');?>
 
    
